59aec3d9445db49881ff0415d80cf833612b8967
DaeRokKim <kdr0112@gmail.com>
2022-07-07 17:54:04 +0900

    220705 C 연습

--- 220705/220705_06.c
+++ 220705/220705_06.c
@@ -2,33 +2,44 @@
 // 드릅게 어렵네
 // 회문 문제
 #include <stdio.h>
+#include <string.h>
 
-void check_Pal(char * ptr) {
-    int len = sizeof(*ptr) / sizeof(char);
-    int i, j=-1;
+void check_pal(char * ptr, int len) {
+    int i, j=-2;
     char temp;
     char * check_p;
 
-    for (i=0; i<len; i++) {
-        temp = ptr[i];
-        check_p[j]=temp;
+    for (i=0; i<len/2; i++) {
+        // temp = ptr[i];
+        // check_p[j] = temp;
+        // j--;
+        if (ptr[i]!=ptr[j]) {
+            printf("%s는 회문이 아닙니다", ptr);
+            return ;
+        }
+        j--;
     }
+    printf("%s는 회문입니다", ptr);
+    return;
 
-    if (*ptr == *check_p) {
-        printf("참");
-    }
-    else {
-        printf("거짓");
-    }
+    // if (*ptr == *check_p) {
+    //     printf("True");
+    // }
+    // else {
+    //     printf("False");
+    // }
 }
 
 int main() {
     char * ptr;
+    int len;
+
+    len = strlen(ptr);
 
     printf("문자열 입력 : ");
     scanf("%s", *ptr);
 
-    check_Pal(ptr);
+    check_pal(ptr, len);
 
     return 0;
 }
