ccaebcaeeacfb7c0d005cd544c9e46f1373fd46e
greenrok <kdr0112@gmail.com>
2022-09-29 17:43:18 +0900

    220929

--- C_practice/220928/LinkedRead.c
+++ C_practice/220928/LinkedRead.c
@@ -31,29 +31,30 @@ int main()
       newNode->data = readData;
       newNode->next = NULL;
 
-      if (tail == NULL)
+      if (head == NULL)
       {
+         head = newNode;
          tail = newNode;
       }
       else
       {
-         head->next = newNode;
+         tail->next = newNode;
       }
 
-      head = newNode;
+      tail = newNode;
    }
 
    printf("\n");
 
    // 입력 받은 데이터의 출력과정
    printf("입력 받은 데이터의 전체출력! \n");
-   if (tail == NULL)
+   if (head == NULL)
    {
       printf("저장된 자연수가 존재하지 않습니다. \n");
    }
    else
    {
-      cur = tail;
+      cur = head;
       printf("%d ", cur->data);
 
       while (cur->next != NULL)
@@ -65,16 +66,16 @@ int main()
    printf("\n\n");
 
    // 메모리의 해제과정
-   if (tail == NULL)
+   if (head == NULL)
    {
       return 0; // 해제할 노드가 존재하지 않음
    }
    else
    {
-      Node *delNode = tail;
-      Node *delNextNode = tail->next;
+      Node *delNode = head;
+      Node *delNextNode = head->next;
 
-      printf("%d을(를) 삭제합니다. \n", tail->data);
+      printf("%d을(를) 삭제합니다. \n", head->data);
       free(delNode); // 첫 번째 노드 삭제
 
       while (delNextNode != NULL) // 두 번째 이후 노드 삭제
